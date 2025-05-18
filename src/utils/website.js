c93fdb2d7ae1e28669cceccc83c44a3e6d396f48
Black Wolf <wolf@blackwolfwoof.com>
2025-05-18 20:53:31 +0200

    sFeature: witched out avatar for entry as it now supports other report types such as users

--- src/utils/website.js
+++ src/utils/website.js
@@ -17,8 +17,8 @@ app.get('/vrchat-report', async (req, res) => {
         }
       });
     }
-    const avatar = await findChannelId(channelId)
-    if (!avatar) {
+    const entry = await findChannelId(channelId)
+    if (!entry) {
       return res.status(401).send({
         error: {
           message: 'Unauthorized',
@@ -29,31 +29,31 @@ app.get('/vrchat-report', async (req, res) => {
 
     let redirectUrl;
 
-    switch (avatar.value.type) {
+    switch (entry.value.type) {
       case "nsfw":
         redirectUrl = `https://help.vrchat.com/hc/en-us/requests/new?ticket_form_id=1500000182242` +
         `&tf_360056455174=user_report` +
         `&tf_anonymous_requester_email=abusereports%40blackwolfwoof.com` +
-        `&tf_1500001445142=${encodeURIComponent(avatar.value.authorDisplayName)}` +
-        `&tf_subject=%5BAvatar%5D%20NSFW%20Avatar%20%22${encodeURIComponent(avatar.value.vrc.name)}%22%20is%20missing%20content_sex%20tag%20%28Automated%20${channelId}%29` +
+        `&tf_1500001445142=${encodeURIComponent(sanitizeText(entry.value.authorDisplayName))}` +
+        `&tf_subject=%5BAvatar%5D%20NSFW%20Avatar%20%22${encodeURIComponent(sanitizeText(entry.value.vrc.name))}%22%20is%20missing%20content_sex%20tag%20%28Automated%20${channelId}%29` +
         `&tf_description=${encodeURIComponent(`The Avatar in question has adult features but is missing the content_sex tag. The attachments may include media showcasing the avatar.<br><br>` +
-          `Avatar ID: ${avatar.id}<br>` +
-          `Avatar Name: ${avatar.value.vrc.name}<br>` +
-          `Avatar Owner ID: ${avatar.value.vrc.authorId}<br>` +
-          `Avatar Owner Name: ${avatar.value.authorDisplayName}<br><br>` +
+          `Avatar ID: ${entry.id}<br>` +
+          `Avatar Name: ${sanitizeText(entry.value.vrc.name)}<br>` +
+          `Avatar Owner ID: ${entry.value.vrc.authorId}<br>` +
+          `Avatar Owner Name: ${sanitizeText(entry.value.authorDisplayName)}<br><br>` +
           `<b>This is a semi automated report. For issues please contact wolf@blackwolfwoof.com</b>`)}`;
         break
       case "crasher":
         redirectUrl = `https://help.vrchat.com/hc/en-us/requests/new?ticket_form_id=1500000182242` +
         `&tf_360056455174=user_report` +
         `&tf_anonymous_requester_email=abusereports%40blackwolfwoof.com` +
-        `&tf_1500001445142=${encodeURIComponent(avatar.value.authorDisplayName)}` +
-        `&tf_subject=%5BAvatar%5D%20Crasher%20Avatar%20%22${encodeURIComponent(avatar.value.vrc.name)}%22%20%28Automated%20${channelId}%29` +
+        `&tf_1500001445142=${encodeURIComponent(sanitizeText(entry.value.authorDisplayName))}` +
+        `&tf_subject=%5BAvatar%5D%20Crasher%20Avatar%20%22${encodeURIComponent(sanitizeText(entry.value.vrc.name))}%22%20%28Automated%20${channelId}%29` +
         `&tf_description=${encodeURIComponent(`The Avatar in question disrupts the gameplay by crashing/lagging the client of the users. The attachments may include media showcasing the avatar.<br><br>` +
-          `Avatar ID: ${avatar.id}<br>` +
-          `Avatar Name: ${avatar.value.vrc.name}<br>` +
-          `Avatar Owner ID: ${avatar.value.vrc.authorId}<br>` +
-          `Avatar Owner Name: ${avatar.value.authorDisplayName}<br><br>` +
+          `Avatar ID: ${entry.id}<br>` +
+          `Avatar Name: ${sanitizeText(entry.value.vrc.name)}<br>` +
+          `Avatar Owner ID: ${entry.value.vrc.authorId}<br>` +
+          `Avatar Owner Name: ${sanitizeText(entry.value.authorDisplayName)}<br><br>` +
           `<b>This is a semi automated report. For issues please contact wolf@blackwolfwoof.com</b>`)}`;
         break
       default:
