f687e417408e577c03fa35134b239d84ccd171f4
Black Wolf <wolf@blackwolfwoof.com>
2025-05-28 06:37:22 +0200

    Feature: Add display name of user to thread

--- src/discord/commands/report-avatar.js
+++ src/discord/commands/report-avatar.js
@@ -174,7 +174,7 @@ async function execute(interaction) {
   if (channel) {
     // Create form thread
     const thread = await channel.threads.create({
-      name: avatar.name,
+      name: `"${avatar.name}" by ${sanitizeText(escapeMarkdown(userInfo?.displayName))}`,
       message: {
         embeds: [embed, embedAvi]
       }
